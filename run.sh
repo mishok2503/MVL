3f500b055d977c628b8d7977b8b5a7b4e795f01f
Mikhail Petrochenkov <mishok2503@mail.ru>
2021-10-25 17:28:08 +0300

    done

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 bison -d parser.ypp &&
 flex lex.l &&
 g++ -std=c++20 parser.tab.cpp lex.yy.c astout.cpp &&
-./a.out t.mvl > ast.out &&
-cat ast.out | jq
+./a.out "$1" > ast.out &&
+jq . ast.out > "$1.out"
